81dbbf9e5834b88150fd5068ba8134397543b124
Denys Makarov <dmakarov@cvent.com>
2016-09-19 17:23:13 +0300

    initial commit

---
+++ hello world/src/main/java/com/denm/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.denm.helloworld;
+
+import java.awt.print.Printable;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world !!!");
+	}
+
+}
